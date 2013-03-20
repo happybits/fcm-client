5c9a29bd6fd396e1b1f6cc33c338d1c7bb620069
Sardar Yumatov <ja.doma@gmail.com>
2013-03-20 10:58:20 +0100

    Distutils fixes

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
     name='gcm-client',
-    version='0.1',
+    version='0.1.1',
     author='Sardar Yumatov',
     author_email='ja.doma@gmail.com',
     url='https://bitbucket.org/sardarnl/gcm-client',
     description='Python client for Google Cloud Messaging (GCM)',
     long_description=open('README.rst').read(),
     packages=['gcmclient'],
-    license=open('LICENSE').read(),
+    license="Apache 2.0",
     keywords='gcm push notification google cloud messaging android',
-    requires=['requests'],
-    platforms = ('Any',),
+    install_requires=['requests'],
     classifiers = [ 'Development Status :: 4 - Beta',
                     'Intended Audience :: Developers',
                     'License :: OSI Approved :: Apache Software License',
                     'Programming Language :: Python',
-                    'Topic :: Software Development :: Libraries :: Python Modules',
-                    ],
+                    'Topic :: Software Development :: Libraries :: Python Modules']
 )
