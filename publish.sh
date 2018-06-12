cdac63286d58f0f38f2543e27677e98faedf92a8
John Loehrer <john@happybits.co>
2018-06-12 13:50:29 -0700

    setting up fcm publish script.

---
+++ publish.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+repo="$1"
+
+if [ -z "$repo" ]
+then
+    repo="pypi"
+fi
+
+make cleanall || exit 1
+
+python2 setup.py sdist bdist_wheel || exit 1
+python3 setup.py sdist bdist_wheel || exit 1
+
+twine upload -r "$repo" dist/* || exit 1
+
